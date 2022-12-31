df56702ef91aeccb5d6ae2a1a2ac9bed87b3cb2b
정대현 <eogus_2513@dsm.hs.kr>
2022-12-31 20:54:17 +0900

    Project :: 프로젝트 세팅

---
+++ timetable-infrastructure/src/main/kotlin/com/xquare/v1servicetimetable/V1ServiceTimetableApplication.kt
@@ -0,0 +1,11 @@
+package com.xquare.v1servicetimetable
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class V1ServiceTimetableApplication
+
+fun main(args: Array<String>) {
+    runApplication<V1ServiceTimetableApplication>(*args)
+}
