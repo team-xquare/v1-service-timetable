50a8089f372cb42a099f0e7dd4482f648080e505
정대현 <eogus_2513@dsm.hs.kr>
2023-01-10 14:15:17 +0900

    Add :: FeignErrorDecoder 설정

---
+++ timetable-infrastructure/src/main/kotlin/com/xquare/v1servicetimetable/adapter/out/thirdparty/feign/FeignClientErrorDecoder.kt
@@ -0,0 +1,25 @@
+package com.xquare.v1servicetimetable.adapter.out.thirdparty.feign
+
+import com.xquare.v1servicetimetable.adapter.out.thirdparty.feign.error.FeignBadRequestException
+import com.xquare.v1servicetimetable.adapter.out.thirdparty.feign.error.FeignExpiredTokenException
+import com.xquare.v1servicetimetable.adapter.out.thirdparty.feign.error.FeignForbiddenException
+import com.xquare.v1servicetimetable.adapter.out.thirdparty.feign.error.FeignUnAuthorizedException
+import feign.FeignException
+import feign.Response
+import feign.codec.ErrorDecoder
+
+class FeignClientErrorDecoder : ErrorDecoder {
+
+    override fun decode(methodKey: String, response: Response): Exception {
+        if (response.status() >= 400) {
+            when (response.status()) {
+                401 -> throw FeignUnAuthorizedException
+                403 -> throw FeignForbiddenException
+                419 -> throw FeignExpiredTokenException
+                else -> throw FeignBadRequestException
+            }
+        }
+
+        return FeignException.errorStatus(methodKey, response)
+    }
+}
