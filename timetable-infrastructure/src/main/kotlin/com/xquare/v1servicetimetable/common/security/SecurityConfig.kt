0111429a41df80a980a5af3e6df5471cdcf5f23b
정대현 <eogus_2513@dsm.hs.kr>
2023-01-10 09:13:58 +0900

    Fix :: @Configuration -> @EnableWebSecurity로 수정

--- timetable-infrastructure/src/main/kotlin/com/xquare/v1servicetimetable/common/security/SecurityConfig.kt
+++ timetable-infrastructure/src/main/kotlin/com/xquare/v1servicetimetable/common/security/SecurityConfig.kt
@@ -1,12 +1,12 @@
 package com.xquare.v1servicetimetable.common.security
 
 import org.springframework.context.annotation.Bean
-import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 
-@Configuration
+@EnableWebSecurity
 class SecurityConfig {
 
     @Bean
