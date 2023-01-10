8b7b5dc07f96236e392c44303f043e0a1e3f953f
정대현 <eogus_2513@dsm.hs.kr>
2023-01-10 09:00:20 +0900

    Fix :: 기본적인 DI 설정 및 에러 해결

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,10 +9,8 @@ subprojects {
     }
 
     dependencies {
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-        testImplementation("org.springframework.boot:spring-boot-starter-test")
     }
 }
 
--- timetable-infrastructure/build.gradle.kts
+++ timetable-infrastructure/build.gradle.kts
@@ -5,12 +5,52 @@ plugins {
     kotlin("plugin.jpa") version "1.6.21"
 }
 
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:2021.0.5")
+    }
+}
+
 dependencies {
+    // Web
     implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // Validation
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+
+    // Security
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    // JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // MySQL
+    runtimeOnly("mysql:mysql-connector-java")
+
+    // Cloud Config
+    implementation("org.springframework.cloud:spring-cloud-starter-config")
+
+    // Open Feign
+    implementation("org.springframework.cloud:spring-cloud-starter-openfeign")
+
+    // Jackson
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     implementation(project(":timetable-application"))
 }
 
 tasks.getByName<Jar>("jar") {
     enabled = false
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
+noArg {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
 }
