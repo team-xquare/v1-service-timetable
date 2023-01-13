d4d8e1a76ec47b90d3cbb8d94272f20b2dcbb056
정대현 <eogus_2513@dsm.hs.kr>
2023-01-13 13:08:55 +0900

    Remove :: 불필요한 Config 파일 삭제

--- timetable-infrastructure/src/main/kotlin/com/xquare/v1servicetimetable/common/config/JpaAuditingConfig.kt
+++
@@ -1,8 +0,0 @@
-package com.xquare.v1servicetimetable.common.config
-
-import org.springframework.context.annotation.Configuration
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-
-@EnableJpaAuditing
-@Configuration
-class JpaAuditingConfig
