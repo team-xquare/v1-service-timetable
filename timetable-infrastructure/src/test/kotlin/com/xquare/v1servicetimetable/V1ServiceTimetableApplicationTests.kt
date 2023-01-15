c425b5be456e97362d6e0dc0ac0ab4a8178e4536
정대현 <eogus_2513@dsm.hs.kr>
2023-01-15 11:53:41 +0900

    Update :: 임시 주석처리

--- timetable-infrastructure/src/test/kotlin/com/xquare/v1servicetimetable/V1ServiceTimetableApplicationTests.kt
+++ timetable-infrastructure/src/test/kotlin/com/xquare/v1servicetimetable/V1ServiceTimetableApplicationTests.kt
@@ -1,13 +1,13 @@
 package com.xquare.v1servicetimetable
 
-import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
-
-@SpringBootTest
-class V1ServiceTimetableApplicationTests {
-
-    @Test
-    fun contextLoads() {
-    }
-
-}
+//import org.junit.jupiter.api.Test
+//import org.springframework.boot.test.context.SpringBootTest
+//
+//@SpringBootTest
+//class V1ServiceTimetableApplicationTests {
+//
+//    @Test
+//    fun contextLoads() {
+//    }
+//
+//}
