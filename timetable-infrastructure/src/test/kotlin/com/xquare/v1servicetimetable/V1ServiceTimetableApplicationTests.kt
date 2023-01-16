5300c9c3c4151623d082ac57217b6ff96a01e131
정대현 <eogus_2513@dsm.hs.kr>
2023-01-17 08:47:42 +0900

    fix :: 불필요한 주석 수정

--- timetable-infrastructure/src/test/kotlin/com/xquare/v1servicetimetable/V1ServiceTimetableApplicationTests.kt
+++ timetable-infrastructure/src/test/kotlin/com/xquare/v1servicetimetable/V1ServiceTimetableApplicationTests.kt
@@ -2,12 +2,12 @@ package com.xquare.v1servicetimetable
 
 //import org.junit.jupiter.api.Test
 //import org.springframework.boot.test.context.SpringBootTest
-//
+
 //@SpringBootTest
 //class V1ServiceTimetableApplicationTests {
-//
+
 //    @Test
 //    fun contextLoads() {
 //    }
-//
+
 //}
