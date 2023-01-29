9b6cb4cfcc1f1953c62e08dce08f541e08a17c1c
정대현 <eogus_2513@dsm.hs.kr>
2023-01-29 18:32:00 +0900

    Update :: LocalDateTime -> LocalTime으로 변경

--- timetable-application/src/main/kotlin/com/xquare/v1servicetimetable/domain/Timetable.kt
+++ timetable-application/src/main/kotlin/com/xquare/v1servicetimetable/domain/Timetable.kt
@@ -3,14 +3,15 @@ package com.xquare.v1servicetimetable.domain
 import com.xquare.v1servicetimetable.common.annotation.Aggregate
 import java.time.LocalDate
 import java.time.LocalDateTime
+import java.time.LocalTime
 import java.util.*
 
 @Aggregate
 class Timetable(
     val id: UUID,
     val period: Int,
-    val beginTime: LocalDateTime,
-    val endTime: LocalDateTime,
+    val beginTime: LocalTime,
+    val endTime: LocalTime,
     val weekDay: Int,
     val date: LocalDate,
     val grade: Int,
