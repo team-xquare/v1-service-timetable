59ee2c148e2cd0b7ccd87b37ef80692567c006bf
정대현 <eogus_2513@dsm.hs.kr>
2023-01-29 22:58:17 +0900

    Remove :: 불필요한 코드 삭제

--- timetable-infrastructure/src/main/kotlin/com/xquare/v1servicetimetable/adapter/out/thirdparty/feign/client/NeisClient.kt
+++ timetable-infrastructure/src/main/kotlin/com/xquare/v1servicetimetable/adapter/out/thirdparty/feign/client/NeisClient.kt
@@ -7,16 +7,6 @@ import org.springframework.web.bind.annotation.RequestParam
 @FeignClient(name = "NeisClient", url = "\${neis.url}")
 interface NeisClient {
 
-    @GetMapping("/SchoolSchedule")
-    fun getSchoolSchedule(
-        @RequestParam("KEY") key: String,
-        @RequestParam("Type") type: String,
-        @RequestParam("pSize") pSize: Int,
-        @RequestParam("ATPT_OFCDC_SC_CODE") regionCode: String,
-        @RequestParam("SD_SCHUL_CODE") schoolCode: String,
-        @RequestParam("AA_FROM_YMD") startDate: String
-    ): String
-
     @GetMapping("/hisTimetable")
     fun getTimetable(
         @RequestParam("KEY") key: String,
