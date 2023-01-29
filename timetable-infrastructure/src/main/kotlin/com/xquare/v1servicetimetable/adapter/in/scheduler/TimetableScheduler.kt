b61700ce003c8908d90b9346ba76a81e43923f12
정대현 <eogus_2513@dsm.hs.kr>
2023-01-29 19:06:20 +0900

    Fix :: PR Comment 수정

--- timetable-infrastructure/src/main/kotlin/com/xquare/v1servicetimetable/adapter/in/scheduler/TimetableScheduler.kt
+++ timetable-infrastructure/src/main/kotlin/com/xquare/v1servicetimetable/adapter/in/scheduler/TimetableScheduler.kt
@@ -2,17 +2,20 @@ package com.xquare.v1servicetimetable.adapter.`in`.scheduler
 
 import com.xquare.v1servicetimetable.adapter.`in`.cron.TimetableCron
 import com.xquare.v1servicetimetable.adapter.out.persistance.domain.TimetableEntity
+import com.xquare.v1servicetimetable.adapter.out.persistance.exception.ConfigNotFoundException
 import com.xquare.v1servicetimetable.adapter.out.persistance.exception.SubjectNotFoundException
+import com.xquare.v1servicetimetable.adapter.out.persistance.repository.ConfigRepository
 import com.xquare.v1servicetimetable.adapter.out.persistance.repository.SubjectRepository
 import com.xquare.v1servicetimetable.adapter.out.persistance.repository.TimetableRepository
 import com.xquare.v1servicetimetable.domain.TableType
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
-import java.time.LocalDate
-import java.time.LocalDateTime
+import java.time.LocalTime
 
 @Component
 class TimetableScheduler(
+    private val configRepository: ConfigRepository,
     private val timetableCron: TimetableCron,
     private val subjectRepository: SubjectRepository,
     private val timetableRepository: TimetableRepository
@@ -22,51 +25,51 @@ class TimetableScheduler(
         const val END_TIME = "END_TIME"
     }
 
-    //@Scheduled(cron = "* * * * * *", zone = "Asia/Seoul") TODO: cron 날짜 정하기(1학기, 2학기)
     @Transactional
+    @Scheduled(cron = "0 0 23 * * 6", zone = "Asia/Seoul")
     fun timetableScheduler() {
+        val config = configRepository.findById(1)
+            .orElseThrow { ConfigNotFoundException }
+        timetableRepository.deleteAll()
+
         val subjectEntityList = subjectRepository.findAll()
-
         for (i in 1..3) {
-            for (j in 1..4) {
+            for (j in 1..config.classCount) {
                 val timetableEntityList: List<TimetableEntity> =
-                    timetableCron.cron(grade = i.toString(), `class` = j.toString())
-                    .map { timetable ->
-                        val subjectEntity = subjectEntityList.find { it.name == timetable.subject }
-                            ?: throw SubjectNotFoundException
+                    timetableCron.timetableCron(grade = i.toString(), `class` = j.toString())
+                        .map { timetable ->
+                            val subjectEntity = subjectEntityList.find { it.name == timetable.subject }
+                                ?: throw SubjectNotFoundException
 
-                        TimetableEntity(
-                            period = timetable.period,
-                            beginTime = getPeriodTime(START_TIME, timetable.period),
-                            endTime = getPeriodTime(END_TIME, timetable.period),
-                            weekDay = timetable.date.dayOfWeek.value,
-                            date = timetable.date,
-                            grade = i,
-                            classNumber = j,
-                            type = TableType.DEFAULT,
-                            subjectEntity = subjectEntity
-                        )
-                    }
+                            TimetableEntity(
+                                period = timetable.period,
+                                beginTime = getPeriodTime(START_TIME, timetable.period),
+                                endTime = getPeriodTime(END_TIME, timetable.period),
+                                weekDay = timetable.date.dayOfWeek.value,
+                                date = timetable.date,
+                                grade = i,
+                                classNumber = j,
+                                type = TableType.DEFAULT,
+                                subjectEntity = subjectEntity
+                            )
+                        }
                 timetableRepository.saveAll(timetableEntityList)
             }
         }
     }
 
-    private fun getPeriodTime(type: String, period: Int): LocalDateTime {
-        val year: Int = LocalDate.now().year
-
-        return if (type == START_TIME) {
+    private fun getPeriodTime(type: String, period: Int): LocalTime =
+        if (type == START_TIME) {
             if (period <= 4) {
-                LocalDateTime.of(year, 1, 1, period + 7, 40)
+                LocalTime.of(period + 7, 40)
             } else {
-                LocalDateTime.of(year, 1, 1, period + 8, 30)
+                LocalTime.of(period + 8, 30)
             }
         } else {
             if (period <= 4) {
-                LocalDateTime.of(year, 1, 1, period + 8, 30)
+                LocalTime.of(period + 8, 30)
             } else {
-                LocalDateTime.of(year, 1, 1, period + 9, 20)
+                LocalTime.of(period + 9, 20)
             }
         }
-    }
 }
