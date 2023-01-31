d2ba072751c999ac1d6f4cab380e1dfdfe1c29b7
정대현 <eogus_2513@dsm.hs.kr>
2023-01-31 11:33:16 +0900

    Add :: TimeNotFoundException 추가

--- timetable-application/src/main/kotlin/com/xquare/v1servicetimetable/common/error/ErrorCode.kt
+++ timetable-application/src/main/kotlin/com/xquare/v1servicetimetable/common/error/ErrorCode.kt
@@ -15,7 +15,11 @@ enum class ErrorCode(
     SUBJECT_NOT_FOUND(statusCode = 404, errorMessage = "Subject Not Found."),
 
     // Config
+    CONFIG_BAD_REQUEST(statusCode = 400, errorMessage = "Bad Request."),
     CONFIG_NOT_FOUND(statusCode = 404, errorMessage = "Config Not Found."),
+
+    // Time
+    TIME_NOT_FOUND(statusCode = 404, errorMessage = "Time Not Found."),
 
     // Etc
     INTERNAL_SERVER_ERROR(statusCode = 500, errorMessage = "Internal Server Error.")
