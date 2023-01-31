71bf0c241d7d9e9dc5cb13a2ca5c3fe1d9f4f25a
정대현 <eogus_2513@dsm.hs.kr>
2023-01-31 11:34:05 +0900

    Update :: TimeEntity로 수정

--- timetable-infrastructure/src/main/kotlin/com/xquare/v1servicetimetable/adapter/in/scheduler/TimetableScheduler.kt
+++ timetable-infrastructure/src/main/kotlin/com/xquare/v1servicetimetable/adapter/in/scheduler/TimetableScheduler.kt
@@ -4,21 +4,23 @@ import com.xquare.v1servicetimetable.adapter.`in`.cron.TimetableCron
 import com.xquare.v1servicetimetable.adapter.out.persistance.domain.TimetableEntity
 import com.xquare.v1servicetimetable.adapter.out.persistance.exception.ConfigNotFoundException
 import com.xquare.v1servicetimetable.adapter.out.persistance.exception.SubjectNotFoundException
+import com.xquare.v1servicetimetable.adapter.out.persistance.exception.TimeNotFoundException
 import com.xquare.v1servicetimetable.adapter.out.persistance.repository.ConfigRepository
 import com.xquare.v1servicetimetable.adapter.out.persistance.repository.SubjectRepository
+import com.xquare.v1servicetimetable.adapter.out.persistance.repository.TimeRepository
 import com.xquare.v1servicetimetable.adapter.out.persistance.repository.TimetableRepository
 import com.xquare.v1servicetimetable.domain.TableType
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
-import java.time.LocalTime
 
 @Component
 class TimetableScheduler(
     private val configRepository: ConfigRepository,
     private val timetableCron: TimetableCron,
     private val subjectRepository: SubjectRepository,
-    private val timetableRepository: TimetableRepository
+    private val timetableRepository: TimetableRepository,
+    private val timeRepository: TimeRepository
 ) {
     companion object {
         const val START_TIME = "START_TIME"
@@ -33,6 +35,7 @@ class TimetableScheduler(
         timetableRepository.deleteAll()
 
         val subjectEntityList = subjectRepository.findAll()
+        val timeEntityList = timeRepository.findAllByType(TableType.DEFAULT)
         for (i in 1..3) {
             for (j in 1..config.classCount) {
                 val timetableEntityList: List<TimetableEntity> =
@@ -40,36 +43,21 @@ class TimetableScheduler(
                         .map { timetable ->
                             val subjectEntity = subjectEntityList.find { it.name == timetable.subject }
                                 ?: throw SubjectNotFoundException
+                            val timeEntity = timeEntityList.find { it.period == timetable.period }
+                                ?: throw TimeNotFoundException
 
                             TimetableEntity(
-                                period = timetable.period,
-                                beginTime = getPeriodTime(START_TIME, timetable.period),
-                                endTime = getPeriodTime(END_TIME, timetable.period),
                                 weekDay = timetable.date.dayOfWeek.value,
                                 date = timetable.date,
                                 grade = i,
                                 classNumber = j,
                                 type = TableType.DEFAULT,
-                                subjectEntity = subjectEntity
+                                subjectEntity = subjectEntity,
+                                timeEntity = timeEntity
                             )
                         }
                 timetableRepository.saveAll(timetableEntityList)
             }
         }
     }
-
-    private fun getPeriodTime(type: String, period: Int): LocalTime =
-        if (type == START_TIME) {
-            if (period <= 4) {
-                LocalTime.of(period + 7, 40)
-            } else {
-                LocalTime.of(period + 8, 30)
-            }
-        } else {
-            if (period <= 4) {
-                LocalTime.of(period + 8, 30)
-            } else {
-                LocalTime.of(period + 9, 20)
-            }
-        }
 }
