e2c5b103913d87b2d88351de673f6f57aad0ddc4
정대현 <eogus_2513@dsm.hs.kr>
2023-02-01 10:57:23 +0900

    Merge :: date Fromat 지정

--- timetable-infrastructure/src/main/kotlin/com/xquare/v1servicetimetable/time/in/TimeWebAdapter.kt
+++ timetable-infrastructure/src/main/kotlin/com/xquare/v1servicetimetable/time/in/TimeWebAdapter.kt
@@ -2,6 +2,8 @@ package com.xquare.v1servicetimetable.time.`in`
 
 import com.xquare.v1servicetimetable.time.port.`in`.TimeDrivingPort
 import com.xquare.v1servicetimetable.time.port.`in`.dto.response.TimeResponse
+import org.springframework.format.annotation.DateTimeFormat
+import org.springframework.format.annotation.DateTimeFormat.ISO
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -13,7 +15,9 @@ class TimeWebAdapter(
 ) {
 
     @GetMapping("/{date}")
-    fun getTime(@PathVariable("date") date: String): TimeResponse {
-        return timeDrivingPort.getTime(LocalDate.parse(date))
-    }
+    fun getTime(
+        @PathVariable("date")
+        @DateTimeFormat(iso = ISO.DATE)
+        date: LocalDate
+    ): TimeResponse = timeDrivingPort.getTime(date)
 }
