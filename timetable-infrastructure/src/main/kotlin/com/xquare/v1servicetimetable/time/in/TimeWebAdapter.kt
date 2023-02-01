f76fda36e572411cc4d4581636084c18d6d5686a
정대현 <eogus_2513@dsm.hs.kr>
2023-02-01 11:34:09 +0900

    Merge :: date String type으로 변경

--- timetable-infrastructure/src/main/kotlin/com/xquare/v1servicetimetable/time/in/TimeWebAdapter.kt
+++ timetable-infrastructure/src/main/kotlin/com/xquare/v1servicetimetable/time/in/TimeWebAdapter.kt
@@ -2,8 +2,6 @@ package com.xquare.v1servicetimetable.time.`in`
 
 import com.xquare.v1servicetimetable.time.port.`in`.TimeDrivingPort
 import com.xquare.v1servicetimetable.time.port.`in`.dto.response.TimeResponse
-import org.springframework.format.annotation.DateTimeFormat
-import org.springframework.format.annotation.DateTimeFormat.ISO
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
@@ -15,9 +13,7 @@ class TimeWebAdapter(
 ) {
 
     @GetMapping("/{date}")
-    fun getTime(
-        @PathVariable("date")
-        @DateTimeFormat(iso = ISO.DATE)
-        date: LocalDate
-    ): TimeResponse = timeDrivingPort.getTime(date)
+    fun getTime(@PathVariable("date") date: String): TimeResponse {
+        return timeDrivingPort.getTime(LocalDate.parse(date))
+    }
 }
