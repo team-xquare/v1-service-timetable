96fdb96983b32d93bcb54e5a0e92f221a16c68a7
정대현 <eogus_2513@dsm.hs.kr>
2023-02-10 13:58:06 +0900

    Add :: Timetable 정보 가져오기

--- timetable-infrastructure/src/main/kotlin/com/xquare/v1servicetimetable/timetable/out/TimetableRepository.kt
+++ timetable-infrastructure/src/main/kotlin/com/xquare/v1servicetimetable/timetable/out/TimetableRepository.kt
@@ -3,7 +3,11 @@ package com.xquare.v1servicetimetable.timetable.out
 import com.xquare.v1servicetimetable.timetable.out.TimetableEntity
 import org.springframework.data.repository.CrudRepository
 import org.springframework.stereotype.Repository
+import java.time.LocalDate
 import java.util.*
 
 @Repository
-interface TimetableRepository : CrudRepository<TimetableEntity, UUID>
+interface TimetableRepository : CrudRepository<TimetableEntity, UUID> {
+
+    fun findAllByDate(date: LocalDate): List<TimetableEntity>
+}
