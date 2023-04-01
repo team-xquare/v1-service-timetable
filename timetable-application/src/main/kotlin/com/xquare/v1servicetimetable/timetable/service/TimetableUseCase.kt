63188302c7b5964d8245735e84cd428a3a6fb46e
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2023-04-01 16:14:40 +0900

    Add :: 일주일 시간표 조회 API에 시간 확인 로직 추가

--- timetable-application/src/main/kotlin/com/xquare/v1servicetimetable/timetable/service/TimetableUseCase.kt
+++ timetable-application/src/main/kotlin/com/xquare/v1servicetimetable/timetable/service/TimetableUseCase.kt
@@ -8,9 +8,12 @@ import com.xquare.v1servicetimetable.timetable.port.`in`.dto.response.WeekTimeta
 import com.xquare.v1servicetimetable.timetable.port.out.QueryTimetablePort
 import com.xquare.v1servicetimetable.timetable.port.out.vo.DayTimeElementVO
 import com.xquare.v1servicetimetable.user.port.UserPort
+import java.time.DayOfWeek
 import java.time.LocalDate
+import java.time.LocalTime
+import java.time.temporal.TemporalAdjusters
 import java.time.temporal.WeekFields
-import java.util.*
+import java.util.Locale
 
 @UseCase
 class TimetableUseCase(
@@ -20,8 +23,21 @@ class TimetableUseCase(
 
     override fun getWeekTimetable(): WeekTimetableResponse {
         val user = userPort.getUser(userPort.getCurrentUserId())
-        val start = LocalDate.now().with(WeekFields.of(Locale.KOREA).dayOfWeek(), 2)
-        val end = LocalDate.now().with(WeekFields.of(Locale.KOREA).dayOfWeek(), 6)
+
+        val nowTime = LocalTime.now()
+        val now = LocalDate.now()
+
+        var start = now.with(WeekFields.of(Locale.KOREA).dayOfWeek(), 2)
+        var end = now.with(WeekFields.of(Locale.KOREA).dayOfWeek(), 6)
+
+        val isAfterThursday = now.isAfter(now.with(WeekFields.of(Locale.KOREA).dayOfWeek(), 5))
+        val isAfterFriday = now.isAfter(now.with(WeekFields.of(Locale.KOREA).dayOfWeek(), 6))
+        val isAfterEndTime = nowTime.isAfter(LocalTime.of(20, 30))
+
+        if (isAfterThursday && isAfterEndTime || isAfterFriday) {
+            start = now.with(TemporalAdjusters.next(DayOfWeek.MONDAY))
+            end = now.with(TemporalAdjusters.next(DayOfWeek.FRIDAY))
+        }
 
         val weekTimeElement: List<WeekTimeElement> = queryTimetablePort
             .findTimetableEntitiesByDateBetweenAndGradeAndClassNum(start, end, user.grade, user.classNum)
