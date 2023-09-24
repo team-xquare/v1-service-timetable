5c66e405b0683fc4d75de840f2e8bb238781b610
이정호 <dlwjdgh1209@dsm.hs.kr>
2023-09-24 14:35:05 +0900

    ♻️ :: config 제거

--- timetable-application/src/main/kotlin/com/xquare/v1servicetimetable/config/exception/ConfigNotFoundException.kt
+++
@@ -1,6 +0,0 @@
-package com.xquare.v1servicetimetable.config.exception
-
-import com.xquare.v1servicetimetable.common.error.BaseException
-import com.xquare.v1servicetimetable.common.error.ErrorCode
-
-object ConfigNotFoundException : BaseException(ErrorCode.CONFIG_NOT_FOUND)
--- timetable-infrastructure/src/main/kotlin/com/xquare/v1servicetimetable/config/out/ConfigRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.xquare.v1servicetimetable.config.out
-
-import org.springframework.data.repository.CrudRepository
-import org.springframework.stereotype.Repository
-
-@Repository
-interface ConfigRepository : CrudRepository<ConfigEntity, Int>
